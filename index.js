f210090502fc78bc72f5297c993f39cfdcefdeb8
usmanisnot <usmanisnot@gmail.com>
2021-04-29 09:51:07 +0500

    get error

--- index.js
+++ index.js
@@ -35,6 +35,22 @@ app.all("/*", function (req, res, next) {
   }
 });
 
+app.get("/*", function (req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  // Set custom headers for CORS
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-type,Accept,X-Access-Token,X-Key"
+  );
+  if (req.method == "OPTIONS") {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+
 // app.get("/", function(req, res) {
 //   res.send(" Real time POS web app running.");
 // });
