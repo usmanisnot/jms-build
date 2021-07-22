2fdc9176b3e769e4b56759def408b3db46cc4808
usmanisnot <usmanisnot@gmail.com>
2021-07-22 15:54:31 +0500

    build

---
+++ react-pos/build/precache-manifest.90127434fbe81baca83cd7d303534a97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a779381572bfa0cbfef2a2ccbb0f19",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c47e9fd92521e9c9000",
+    "url": "./static/css/2.4019c64b.chunk.css"
+  },
+  {
+    "revision": "cee29deb2e204f4f6c7f",
+    "url": "./static/css/main.7a7f5a33.chunk.css"
+  },
+  {
+    "revision": "9c47e9fd92521e9c9000",
+    "url": "./static/js/2.6a13d27c.chunk.js"
+  },
+  {
+    "revision": "cee29deb2e204f4f6c7f",
+    "url": "./static/js/main.5180335b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
